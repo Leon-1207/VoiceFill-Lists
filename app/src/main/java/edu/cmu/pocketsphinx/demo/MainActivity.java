42f3d88aa6b73f818188bc1b765ed929f494aa6d
Leon <leon.mainusch@web.de>
2023-08-15 16:58:25 +0200

    NEW: compare after value update (WIP)

--- app/src/main/java/edu/cmu/pocketsphinx/demo/MainActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/MainActivity.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import edu.cmu.pocketsphinx.Hypothesis;
+
 public class MainActivity extends PhoneRecognizerActivity {
 
     private TextView firstRowText, secondRowText, thirdRowText;
-    private Button firstRowButton, secondRowButton, thirdRowButton;
+    private Button firstRowButton, secondRowButton;
+    private PhoneStringComparator comparator;
 
     private String firstValue, secondValue;
     private int recordingIndex;
@@ -25,7 +28,8 @@ public class MainActivity extends PhoneRecognizerActivity {
 
         firstRowButton = findViewById(R.id.firstRowButton);
         secondRowButton = findViewById(R.id.secondRowButton);
-        thirdRowButton = findViewById(R.id.thirdRowButton);
+
+        comparator = new PhoneStringComparator();
 
         // reset values
         firstValue = "";
@@ -36,9 +40,9 @@ public class MainActivity extends PhoneRecognizerActivity {
             @Override
             public void onClick(View v) {
                 // Record button in the first row is clicked
-                if (recordingIndex == 0) stopRecording(0);
+                if (recordingIndex == 0) stopRecording();
                 else {
-                    if (recordingIndex == 1) stopRecording(1);
+                    if (recordingIndex == 1) stopRecording();
                     startRecording(0);
                 }
             }
@@ -48,26 +52,43 @@ public class MainActivity extends PhoneRecognizerActivity {
             @Override
             public void onClick(View v) {
                 // Record button in the second row is clicked
-                if (recordingIndex == 1) stopRecording(1);
+                if (recordingIndex == 1) stopRecording();
                 else {
-                    if (recordingIndex == 0) stopRecording(0);
+                    if (recordingIndex == 0) stopRecording();
                     startRecording(1);
                 }
             }
         });
-
-        thirdRowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Compare button in the third row is clicked
-                // TODO
-            }
-        });
     }
 
-    private void stopRecording(int index) {
-        String result = "TODO";
+    private void stopRecording() {
+        stopListening();
+    }
 
+    @Override
+    public void stopListening() {
+        super.stopListening();
+        if (recordingIndex != -1) {
+            TextView textView = (recordingIndex == 0) ? firstRowText : secondRowText;
+            textView.setText("");
+            textView.setTypeface(null, Typeface.NORMAL);
+
+            Button button = (recordingIndex == 0) ? firstRowButton : secondRowButton;
+            button.setText("Record");
+        }
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+        if (recordingIndex != -1) {
+            if (hypothesis != null) {
+                String text = hypothesis.getHypstr();
+                handleRecordingResult(recordingIndex, text);
+            } else handleRecordingResult(recordingIndex, "");
+        }
+    }
+
+    private void handleRecordingResult(int index, String result) {
         TextView textView = (index == 0) ? firstRowText : secondRowText;
         textView.setText(result);
         textView.setTypeface(null, Typeface.NORMAL);
@@ -75,7 +96,12 @@ public class MainActivity extends PhoneRecognizerActivity {
         Button button = (index == 0) ? firstRowButton : secondRowButton;
         button.setText("Record");
 
+        updateValue(index, result);
         recordingIndex = -1;
+
+        // enable buttons
+        firstRowButton.setEnabled(true);
+        secondRowButton.setEnabled(true);
     }
 
     private void startRecording(int index) {
@@ -87,5 +113,22 @@ public class MainActivity extends PhoneRecognizerActivity {
 
         Button button = (index == 0) ? firstRowButton : secondRowButton;
         button.setText("Stop");
+
+        // disable other button
+        Button otherButton = (index == 0) ? secondRowButton : firstRowButton;
+        otherButton.setEnabled(false);
+
+        startListening();
+    }
+
+    private void updateValue(int index, String value) {
+        if (index == 0) firstValue = value;
+        else if (index == 1) secondValue = value;
+        compareValues();
+    }
+
+    private void compareValues() {
+        float result = comparator.compare(firstValue, secondValue);
+        thirdRowText.setText(String.format("%s", result));
     }
 }
---
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/PhoneStringComparator.java
@@ -0,0 +1,8 @@
+package edu.cmu.pocketsphinx.demo;
+
+public class PhoneStringComparator {
+    public float compare(String a, String b) {
+        // TODO
+        return (float) 0;
+    }
+}
