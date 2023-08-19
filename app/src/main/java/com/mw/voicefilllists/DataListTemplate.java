205e0bca840996aefc5608e2b7cc4ac1248015c2
Leon <leon.mainusch@web.de>
2023-08-19 14:58:08 +0200

    Recognizer activity (WIP)

---
+++ app/src/main/java/com/mw/voicefilllists/DataListTemplate.java
@@ -0,0 +1,5 @@
+package com.mw.voicefilllists;
+
+public class DataListTemplate {
+
+}
---
+++ app/src/main/java/com/mw/voicefilllists/UserDataLoader.java
@@ -0,0 +1,7 @@
+package com.mw.voicefilllists;
+
+import java.util.List;
+
+public interface UserDataLoader {
+    public List<DataListTemplate> loadDataListTemplates();
+}
