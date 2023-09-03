d291f2c7c21513dfb68bf2c0108eae8017a8ef7b
Leon <leon.mainusch@web.de>
2023-09-03 18:30:43 +0200

    NEW: template selection spinner

--- app/src/main/java/com/mw/voicefilllists/activities/DataListPageActivity.java
+++ app/src/main/java/com/mw/voicefilllists/activities/DataListPageActivity.java
@@ -2,13 +2,18 @@ package com.mw.voicefilllists.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mw.voicefilllists.LoadingScreen;
+import com.mw.voicefilllists.NothingSelectedSpinnerAdapter;
 import com.mw.voicefilllists.R;
+import com.mw.voicefilllists.localdb.AppDatabase;
 import com.mw.voicefilllists.localdb.entities.DataListTemplateDatabaseEntry;
 
 import java.util.List;
@@ -16,19 +21,56 @@ import java.util.List;
 public abstract class DataListPageActivity extends AppCompatActivity {
     private LoadingScreen loadingScreen;
     private List<DataListTemplateDatabaseEntry> templates;
+    private Spinner templateSpinner;
+    private EditText nameTextInput;
+    private String initialNameValue;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.loadingScreen = new LoadingScreen(this);
+
         setContentView(R.layout.activity_data_list_page);
+        this.templateSpinner = findViewById(R.id.pageTemplate);
+        this.nameTextInput = findViewById(R.id.pageName);
+
         setupToolbar();
-        loadDataAndStartSetup();
         setupBottomButtonLine();
+        loadDataAndStartSetup();
     }
 
     private void loadDataAndStartSetup() {
+        loadingScreen.show();
+        new Thread(() -> {
+            loadTemplatesFromDatabaseInCurrentThread();
+            loadInitialNameInCurrentThread();
+            runOnUiThread(() -> {
+                setupNameInput();
+                setupTemplateInput();
+                loadingScreen.dismiss();
+            });
+        }).start();
+    }
 
+    private void loadTemplatesFromDatabaseInCurrentThread() {
+        this.templates = AppDatabase.getInstance(this).dataListTemplateDAO().getAll();
+    }
+
+    protected void loadInitialNameInCurrentThread() {
+        this.initialNameValue = "";
+    }
+
+    protected void setupNameInput() {
+        nameTextInput.setText(initialNameValue);
+    }
+
+    protected void setupTemplateInput() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        for (DataListTemplateDatabaseEntry databaseEntry : templates) {
+            adapter.add(databaseEntry.name);
+        }
+        templateSpinner.setAdapter(new NothingSelectedSpinnerAdapter(adapter, R.layout.please_select_an_option_item, this));
     }
 
     private void setupBottomButtonLine() {
@@ -65,4 +107,12 @@ public abstract class DataListPageActivity extends AppCompatActivity {
     }
 
     protected abstract void setupToolbar();
+
+    public Spinner getTemplateSpinner() {
+        return templateSpinner;
+    }
+
+    public EditText getNameTextInput() {
+        return nameTextInput;
+    }
 }
