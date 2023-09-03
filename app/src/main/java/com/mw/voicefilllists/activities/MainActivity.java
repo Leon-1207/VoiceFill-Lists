80f78b3f58bd158c8c8c1cb4c6834dd0e7f8a3a0
Leon <leon.mainusch@web.de>
2023-09-03 20:46:41 +0200

    FIX: optimize imports

--- app/src/main/java/com/mw/voicefilllists/activities/MainActivity.java
+++ app/src/main/java/com/mw/voicefilllists/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
